e624beaa2b8048595ca03cb4e2a3d036eb24b347
D7703_13 <d7703_13@d7703-13ui-imac.local>
2019-03-26 15:36:24 +0900

    2 time commit

--- imgslide/ViewController.swift
+++ imgslide/ViewController.swift
@@ -10,22 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var myImg: UIImageView!
+    @IBOutlet weak var mylabel: UILabel!
+    
     var count = 1
+    var direction = true //up
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         myImg.image = UIImage(named: "frame1.png")
+        mylabel.text = "frame1.png"
     }
 
     @IBAction func changeImg(_ sender: Any) {
-        count = count + 1
+        if count == 5 {
+            direction = false
+        } else if count == 1 {
+            direction = true
+        }
+        if direction == true {
+            count += 1
+        } else {
+            count -= 1
+        }
         
         myImg.image = UIImage(named: "frame\(count).png")
-        
-        if count == 5{
-            count = 0;
-        }
+        mylabel.text = "frame\(count).png"
     }
     
 }
