c82468d65d27fe46ab013334e077ddda0ce3c516
D7703_13 <d7703_13@d7703-13ui-imac.local>
2019-03-26 15:28:02 +0900

    commit

--- imgslide/ViewController.swift
+++ imgslide/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImg: UIImageView!
+    var count = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myImg.image = UIImage(named: "frame1.png")
     }
 
-
+    @IBAction func changeImg(_ sender: Any) {
+        count = count + 1
+        
+        myImg.image = UIImage(named: "frame\(count).png")
+        
+        if count == 5{
+            count = 0;
+        }
+    }
+    
 }
 
